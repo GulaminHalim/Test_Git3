32983176b0b37623009c499c2708d82d5f03fb32
GulaminHalim <Halimsrgr@gmail.com>
2023-07-14 22:59:54 -0700

    Feat: delete tag H3

--- src/module/landingPage/sections/WhyUs.js
+++ src/module/landingPage/sections/WhyUs.js
@@ -15,7 +15,7 @@ export default function WhyUs(){
         <br></br>
         <br></br>
 
-        <h1 className="hero-section-text-title">Why Us</h1><h3></h3>
+        <h1 className="hero-section-text-title">Why Us</h1>
         <p style={{fontWeight: 'bold'}}>Mengapa harus pilih Binar Car Rental?</p>
         <Row style={{gap: 16}}>
             <Col>
